e5f3106743825bcebbb0112aeb4d1d5fa320e225
Richard J <richjim89@gmail.com>
2018-10-08 11:47:47 -0700

    Added console lines for better usability

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,28 +6,48 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Adder(7, 4));
-            Console.WriteLine(Subtract(5, 7));
-            Console.WriteLine(Mult(5, 3));
-            Console.WriteLine(Divide(8, 2));
+            Console.WriteLine("What calculator function would you like to perform?");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+
+            string selection = Console.ReadLine();
+
+            Console.WriteLine("Input two numbers to calculate your final result:");
+            int firstNum = Int32.Parse(Console.ReadLine());
+            int secondNum = Int32.Parse(Console.ReadLine());
+
+            if (selection == "1")
+            {
+                int result = Adder(firstNum, secondNum);
+                Console.WriteLine(result);
+            }
+
+
+
             Console.ReadLine();
         }
 
+        //Method to add numbers
         static int Adder(int num1, int num2)
         {
             return num1 + num2;
         }
 
+        //Method to subtract numbers
         static int Subtract(int num1, int num2)
         {
             return num1 - num2;
         }
 
-        static int Mult(int num1, int num2)
+        //Method to multiply numbers
+        static int Multiply(int num1, int num2)
         {
             return num1 * num2;
         }
 
+        //Method to divide numbers
         static int Divide(int num1, int num2)
         {
             return num1 / num2;

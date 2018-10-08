383c36d7119bed5738722023525fe05d99b657c1
Richard J <richjim89@gmail.com>
2018-10-08 11:24:48 -0700

    created multiplication method

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,13 +6,19 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Adder(5, 7));
+            Console.WriteLine(Adder(5, 3));
+            Console.WriteLine(Mult(5, 3));
             Console.ReadLine();
         }
 
         static int Adder(int num1, int num2)
         {
             return num1 + num2;
+        }
+
+        static int Mult(int num1, int num2)
+        {
+            return num1 * num2;
         }
     }
 }

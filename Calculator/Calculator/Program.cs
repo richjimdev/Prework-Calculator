1d3e213eb320bed5d9ac6ff9d12fa7e294b2a880
Richard J <richjim89@gmail.com>
2018-10-08 11:21:38 -0700

    created add method

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Adder(5, 7));
+            Console.ReadLine();
+        }
+
+        static int Adder(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
